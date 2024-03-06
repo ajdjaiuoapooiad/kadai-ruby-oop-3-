01506499ff564a125eae583c1a9411b87273c694
yukito.takeuchi <160101080+ajdjaiuoapooiad@users.noreply.github.com>
2024-03-06 10:56:08 +0900

    Update animal.rb

--- animal.rb
+++ animal.rb
@@ -3,12 +3,12 @@ class Animal
   attr_accessor :name, :old_num
   
   def initialize(name,old_num)
-    @name = name
-    @old_num = old_num
+    self.name = name
+    self.old_num = old_num
   end
   
   def say
-    return "#{@name}です。#{@old_num}歳です。"
+    return "#{self.name}です。#{self.old_num}歳です。"
   end
   
 end
